0f25de8ab0b6b410103a7c5a14e0c21224199667
fin-ch <woody101@snu.ac.kr>
2022-11-30 02:16:43 +0900

    ..

--- svelte/src/routes/proto/display.svelte
+++ svelte/src/routes/proto/display.svelte
@@ -56,7 +56,18 @@
 		</div>
 	{:else}
 		<div class="border-0 mx-6 my-2 rounded-xl grid grid-cols-1 justify-items-center bg-slate-100">
-		<div class="font-bold mx-4 my-2">route data</div>
+            <div class="border-0 mx-6 my-2 rounded-xl grid grid-cols-1 justify-items-center bg-slate-100">
+                <div class="font-bold mx-4 my-2">device data</div>
+                <div class="mx-4 my-2">
+                    distContd: {$data.config.device.distContd}<br>
+                    dircContd: {$data.config.device.dircContd}<br>
+                    distRange: {$data.config.device.distRange}<br>
+                    signCount: {$data.config.device.signCount}<br>
+                    signPoint: {$data.config.device.signPoint}<br>
+                    haptics: {$data.config.device.haptics} ({$data.config.device.hapticsIdx})<br>
+                </div>
+                </div>
+        <div class="font-bold mx-4 my-2">route data</div>
 		<div class="mx-4 my-2">
 			startPoint ... ({$data.config.route.startPoint.location.lat}, {$data.config.route.startPoint.location.lon})<br>
 			endPoint ... ({$data.config.route.endPoint.location.lat}, {$data.config.route.endPoint.location.lon})<br>
@@ -70,30 +81,19 @@
 			{/if}
 		</div>
 		</div>
-		<div class="border-0 mx-6 my-2 rounded-xl grid grid-cols-1 justify-items-center bg-slate-100">
-		<div class="font-bold mx-4 my-2">device data</div>
-		<div class="mx-4 my-2">
-			distContd: {$data.config.device.distContd}<br>
-			dircContd: {$data.config.device.dircContd}<br>
-			distRange: {$data.config.device.distRange}<br>
-			signCount: {$data.config.device.signCount}<br>
-			signPoint: {$data.config.device.signPoint}<br>
-			haptics: {$data.config.device.haptics} ({$data.config.device.hapticsIdx})<br>
-		</div>
-		</div>
 	{/if}
 {/if}
 
 <div class="border-0 mx-6 my-2 rounded-xl grid grid-cols-1 justify-items-center place-content-center bg-slate-100">
 	<div class="mx-4 my-0.5 grid grid-cols-1 content-start">
 	{#each config_lst as i}
-    	<button class="mx-4 my-2 py-0.5 px-4 w-64 rounded-lg shadow-md text-white bg-blue-500" on:click={ async () => {const res = await fetch(`http://54.180.122.164:3000/updatedb/config/` + i, {method: 'POST'});} }>Update Config {i}</button>
+    	<button class="mx-4 my-2 py-0.5 px-4 w-36 rounded-lg shadow-md text-white bg-blue-500" on:click={ async () => {const res = await fetch(`http://54.180.122.164:3000/updatedb/config/` + i, {method: 'POST'});} }>Update Config {i}</button>
 	{/each}
 	</div>
 	<div class="mx-4 my-0.5 grid grid-cols-1 content-start">
-		<button class="mx-4 my-2 py-0.5 px-4 w-64 rounded-lg shadow-md text-white bg-blue-500" on:click={ async () => {const res = await fetch(`http://54.180.122.164:3000/updatedb/calib/`, {method: 'POST', body:JSON.stringify($storeFE), headers:{'Content-Type': 'application/json'}});} }>Config Calib</button>
-		<button class="mx-4 my-2 py-0.5 px-4 w-64 rounded-lg shadow-md text-white bg-blue-500" on:click={ async () => {const res = await fetch(`http://54.180.122.164:3000/updatedb/resetdestination/`, {method: 'POST'});} }>Reset Destination</button>
-		<button class="mx-4 my-2 py-0.5 px-4 w-64 rounded-lg shadow-md text-white bg-blue-500" on:click={ async () => {const res = await fetch(`http://54.180.122.164:3000/updatedb/setdestination/`, {method: 'POST'});} }>Set Destination</button>
+		<button class="mx-4 my-2 py-0.5 px-4 w-36 rounded-lg shadow-md text-white bg-blue-500" on:click={ async () => {const res = await fetch(`http://54.180.122.164:3000/updatedb/calib/`, {method: 'POST', body:JSON.stringify($storeFE), headers:{'Content-Type': 'application/json'}});} }>Config Calib</button>
+		<button class="mx-4 my-2 py-0.5 px-4 w-36 rounded-lg shadow-md text-white bg-blue-500" on:click={ async () => {const res = await fetch(`http://54.180.122.164:3000/updatedb/resetdestination/`, {method: 'POST'});} }>Reset Destination</button>
+		<button class="mx-4 my-2 py-0.5 px-4 w-36 rounded-lg shadow-md text-white bg-blue-500" on:click={ async () => {const res = await fetch(`http://54.180.122.164:3000/updatedb/setdestination/`, {method: 'POST'});} }>Set Destination</button>
 	</div>
 </div>
 </div>
