a08b9753592135047a5a9fe239211167d3c52128
fin-ch <woody101@snu.ac.kr>
2022-11-30 00:12:03 +0900

    ..

--- svelte/vite.config.js
+++ svelte/vite.config.js
@@ -1,11 +1,7 @@
-import postcss from "./postcss.config.cjs";
 import { sveltekit } from "@sveltejs/kit/vite";
 
 const config = {
     plugins: [sveltekit()],
-    css: {
-        postcss,
-    },
 };
 
 export default config;
