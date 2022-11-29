20c40b2ea392fc89a0588e0e49ff125c202cb7c0
fin-ch <woody101@snu.ac.kr>
2022-11-29 15:22:24 +0900

    ..

--- svelte/src/routes/proto/+page.svelte
+++ svelte/src/routes/proto/+page.svelte
@@ -52,12 +52,12 @@
 </script>
 
 <div>
-	Drv 0 ... <input bind:value={ $storeHP[0].pow }><input bind:value={ $storeHP[0].freq }><br>
-	Drv 1 ... <input bind:value={ $storeHP[1].pow }><input bind:value={ $storeHP[1].freq }><br>
-	Drv 2 ... <input bind:value={ $storeHP[2].pow }><input bind:value={ $storeHP[2].freq }><br>
-	Drv 3 ... <input bind:value={ $storeHP[3].pow }><input bind:value={ $storeHP[3].freq }><br>
-	Drv 4 ... <input bind:value={ $storeHP[4].pow }><input bind:value={ $storeHP[4].freq }><br>
-	Drv 5 ... <input bind:value={ $storeHP[5].pow }><input bind:value={ $storeHP[5].freq }><br>
+	<input bind:value={ $storeHP[0].pow }><input bind:value={ $storeHP[0].freq }>
+	<input bind:value={ $storeHP[1].pow }><input bind:value={ $storeHP[1].freq }>
+	<input bind:value={ $storeHP[2].pow }><input bind:value={ $storeHP[2].freq }>
+	<input bind:value={ $storeHP[3].pow }><input bind:value={ $storeHP[3].freq }>
+	<input bind:value={ $storeHP[4].pow }><input bind:value={ $storeHP[4].freq }>
+	<input bind:value={ $storeHP[5].pow }><input bind:value={ $storeHP[5].freq }>
 </div>
 
 <input bind:value={ count }>
