e0edf7c64cdbf62216cd4dd6bbd2f81c59f0cb6e
fin-ch <woody101@snu.ac.kr>
2022-11-30 02:42:29 +0900

    ..

--- svelte/src/routes/proto/display.svelte
+++ svelte/src/routes/proto/display.svelte
@@ -7,7 +7,7 @@
 {#if $data == undefined}
 	<div class="font-bold mx-2 my-2">loading</div>
 {:else}
-	<div class="border-0 mx-2 my-2 rounded-xl grid grid-cols-1 justify-items-center bg-slate-100 min-w-1/2">
+	<div class="border-0 mx-2 my-2 rounded-xl grid grid-cols-1 justify-items-center content-start bg-slate-100 min-w-1/2">
 	{#if $data.gps == undefined}
 		<div class="font-bold mx-2 my-2">loading GPS data</div>
 	{:else}
@@ -21,7 +21,7 @@
 	{/if}
 	</div>
 
-	<div class="border-0 mx-2 my-2 rounded-xl grid grid-cols-1 justify-items-center bg-slate-100 min-w-1/2">
+	<div class="border-0 mx-2 my-2 rounded-xl grid grid-cols-1 justify-items-center content-start bg-slate-100 min-w-1/2">
 	{#if $data.navigate == undefined}
 		<div class="font-bold mx-2 my-2">loading navigate data</div>
 	{:else}
@@ -37,7 +37,7 @@
 	{/if}
 	</div>
 
-	<div class="border-0 mx-2 my-2 rounded-xl grid grid-cols-1 justify-items-center bg-slate-100 min-w-1/2">
+	<div class="border-0 mx-2 my-2 rounded-xl grid grid-cols-1 justify-items-center content-start bg-slate-100 min-w-1/2">
 	{#if $data.haptic == undefined}
 		<div class="font-bold mx-2 my-2">loading haptic data</div>
 	{:else}
@@ -51,11 +51,11 @@
 	</div>
 
 	{#if $data.config == undefined}
-		<div class="border-0 mx-2 my-2 rounded-xl grid grid-cols-1 justify-items-center bg-slate-100 min-w-1/2">
+		<div class="border-0 mx-2 my-2 rounded-xl grid grid-cols-1 justify-items-center content-start bg-slate-100 min-w-1/2">
 		<div class="font-bold mx-2 my-2">loading config data</div>
 		</div>
 	{:else}
-        <div class="border-0 mx-2 my-2 rounded-xl grid grid-cols-1 justify-items-center bg-slate-100 min-w-1/2">
+        <div class="border-0 mx-2 my-2 rounded-xl grid grid-cols-1 justify-items-center content-start bg-slate-100 min-w-1/2">
 		<div class="font-bold mx-2 my-2">device data</div>
 		<div class="mx-2 my-2">
 			distContd: {$data.config.device.distContd}<br>
@@ -66,7 +66,7 @@
 			haptics: {$data.config.device.haptics} ({$data.config.device.hapticsIdx})<br>
 		</div>
 		</div>
-		<div class="border-0 mx-2 my-2 rounded-xl grid grid-cols-1 justify-items-center bg-slate-100 min-w-1/2">
+		<div class="border-0 mx-2 my-2 rounded-xl grid grid-cols-1 justify-items-center content-start bg-slate-100 min-w-1/2">
         <div class="font-bold mx-2 my-2">route data</div>
 		<div class="mx-2 my-2">
 			startPoint ... ({$data.config.route.startPoint.location.lat}, {$data.config.route.startPoint.location.lon})<br>
@@ -84,7 +84,7 @@
 	{/if}
 {/if}
 
-<div class="border-0 mx-2 my-2 rounded-xl grid grid-cols-1 justify-items-center place-content-center bg-slate-100 min-w-1/2">
+<div class="border-0 mx-2 my-2 rounded-xl grid grid-cols-1 justify-items-center content-start bg-slate-100 min-w-1/2">
 	<div class="mx-2 my-0.5 grid grid-cols-1 content-start">
 	{#each config_lst as i}
     	<button class="mx-2 my-2 py-0.5 px-4 w-36 rounded-lg shadow-md text-white bg-blue-500" on:click={ async () => {const res = await fetch(`http://54.180.122.164:3000/updatedb/config/` + i, {method: 'POST'});} }>Update Config {i}</button>
