c1940239a9ef9b7b5489d0b278aa3e807a10ff0a
fin-ch <woody101@snu.ac.kr>
2022-11-30 00:03:56 +0900

    ..

--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -2,11 +2,9 @@
     import { onMount } from 'svelte';
     import { identity } from 'svelte/internal';
     // import users from '../lib/data/db.json';
-    import TailwindCss from '../TailwindCSS.svelte';
+    // import TailwindCss from '../TailwindCSS.svelte';
 </script>
 
-<TailwindCss />
-
-<h1>Welcome to SvelteKit</h1>
+<h1 class="ml-3 text-xl">Welcome to SvelteKit</h1>
 <h2>This will be run on the express server.</h2>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
